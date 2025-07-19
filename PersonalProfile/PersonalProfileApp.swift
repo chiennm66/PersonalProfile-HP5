2554147dd8a1b700f2f464d9d36701f231fd81c8
chienabc <chien97666@gmail.com>
2025-07-19 18:18:42 +0700

    Initial Commit

---
+++ PersonalProfile/PersonalProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalProfileApp.swift
+//  PersonalProfile
+//
+//  Created by CHIEN on 19/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
